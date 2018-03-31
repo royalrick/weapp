23bed463a40bd715ccd06d4f51c8e2ef30741a9d
Medivhzhan <medivhzhan@gmail.com>
2018-03-31 10:59:00 +0800

    获取小程序码后保存文件

--- Readme.md
+++ Readme.md
@@ -18,62 +18,73 @@ import "github.com/medivhzhan/weapp"
 
 ```go
 // 获取次数有限制 获取后请缓存
-accessToken, expire, err := weapp.AccessToken(appID, secret string)
+
+weapp.AccessToken(appID, secret string) (accessToken string, expire uint, err error)
+
 ```
 
 ## 用户登录
 
 ```go
 // 需要从小程序客户端获取到的code
-openID, sessionKey, err := weapp.Login(appID, secret, code string)
+
+weapp.Login(appID, secret, code string) (openID string, sessionKey uint, err error)
+
 ```
 
 ## 获取小程序码: 适用于需要的码数量较少的业务场景
 
 ```go
-// 可接受path参数较长 生成个数受限 永久有效
-// path 识别二维码后进入小程序的页面链接
-// width 图片宽度
-// autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
-// lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
-// token access_token
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-res, err := weapp.AppCode(path string, width int, autoColor bool, lineColor, accessToken string)
-if err != nil {
-    panic(err)
-}
-defer res.Body.Close()
+// AppCode 获取小程序码
+// 可接受path参数较长 生成个数受限 永久有效 适用于需要的码数量较少的业务场景
+// @ path 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+// @ lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
+// @ token 微信access_token
+// @ filename 文件储存路径
+
+weapp.AppCode(path string, width int, autoColor bool, lineColor, accessToken, filename string) error
+
 ```
 
 ## 获取小程序码: 适用于需要的码数量极多的业务场景
 
 ```go
-// 可接受页面参数较短 生成个数不受限
+// UnlimitedAppCode 获取小程序码
+// 可接受页面参数较短 生成个数不受限 适用于需要的码数量极多的业务场景
 // 根路径前不要填加'/' 不能携带参数（参数请放在scene字段里）
-// scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-res, err := weapp.UnlimitedAppCode(scene, path string, width int, autoColor bool, lineColor, accessToken string)
-if err != nil {
-    panic(err)
-}
-defer res.Body.Close()
+// @ scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
+// @ page 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+// @ lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
+// @ token 微信access_token
+// @ filename 文件储存路径
+
+weapp.UnlimitedAppCode(scene, path string, width int, autoColor bool, lineColor, accessToken string) error
+
 ```
 
 ## 获取小程序二维码: 适用于需要的码数量较少的业务场景
 
 ```go
 // QRCode 获取小程序二维码
-// 可接受path参数较长，生成个数受限 永久有效
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-res, err := weapp.QRCode(path string, width int, token string)
-if err != nil {
-    panic(err)
-}
-defer res.Body.Close()
+// 可接受path参数较长，生成个数受限 永久有效 适用于需要的码数量较少的业务场景
+// @ path 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ token 微信access_token
+// @ filename 文件储存路径
+
+weapp.QRCode(path string, width int, token string) error
+
 ```
+
+## 未实现功能
+
+1. 保存图片
+1. 客服消息
+1. 模版消息
+1. 微信支付
+1. 数据统计
+1. 临时素材
--- code.go
+++ code.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
@@ -12,51 +13,74 @@ import (
 const (
 	appCodeAPI          = "/wxa/getwxacode"
 	unlimitedAppCodeAPI = "/wxa/getwxacodeunlimit"
-	QRCodeAPI           = "/cgi-bin/wxaapp/createwxaqrcode"
+	qrCodeAPI           = "/cgi-bin/wxaapp/createwxaqrcode"
 )
 
 // AppCode 获取小程序码
 // 可接受path参数较长 生成个数受限 永久有效 适用于需要的码数量较少的业务场景
-// path 识别二维码后进入小程序的页面链接
-// width 图片宽度
-// autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
-// lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
-// token access_token
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-func AppCode(path string, width int, autoColor bool, lineColor, token string) (res *http.Response, err error) {
+// @ path 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+// @ lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
+// @ token 微信access_token
+// @ filename 文件储存路径
+func AppCode(path string, width int, autoColor bool, lineColor, token, filename string) error {
+
 	body := fmt.Sprintf(`{"path":"%s","width": %v,"auto_color": %v,"line_color": %s}`, path, width, autoColor, lineColor)
 
-	return requestCode(appCodeAPI, body, token)
+	res, err := requestCode(appCodeAPI, body, token)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	return saveCode(res, filename)
 }
 
 // UnlimitedAppCode 获取小程序码
 // 可接受页面参数较短 生成个数不受限 适用于需要的码数量极多的业务场景
 // 根路径前不要填加'/' 不能携带参数（参数请放在scene字段里）
-// scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-func UnlimitedAppCode(scene, page string, width int, autoColor bool, lineColor, token string) (res *http.Response, err error) {
+// @ scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
+// @ page 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ autoColor 自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+// @ lineColor autoColor 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"},十进制表示
+// @ token 微信access_token
+// @ filename 文件储存路径
+func UnlimitedAppCode(scene, page string, width int, autoColor bool, lineColor, token, filename string) error {
+
 	body := fmt.Sprintf(`{"scene": "%s","page":"%s","width": %v,"auto_color": %v,"line_color": %s}`, scene, page, width, autoColor, lineColor)
 
-	return requestCode(unlimitedAppCodeAPI, body, token)
+	res, err := requestCode(unlimitedAppCodeAPI, body, token)
+	if err != nil {
+		return nil
+	}
+	defer res.Body.Close()
+
+	return saveCode(res, filename)
 }
 
 // QRCode 获取小程序二维码
 // 可接受path参数较长，生成个数受限 永久有效 适用于需要的码数量较少的业务场景
-// 返回小程序码HTTP请求
-// 请记得关闭资源
-// 获取后请注意保存到本地以减少请求次数
-func QRCode(path string, width int, token string) (res *http.Response, err error) {
+// @ path 识别二维码后进入小程序的页面链接
+// @ width 图片宽度
+// @ token 微信access_token
+// @ filename 文件储存路径
+func QRCode(path string, width int, token, filename string) error {
 
 	body := fmt.Sprintf(`{"path":"%s","width": %v}`, path, width)
 
-	return requestCode(QRCodeAPI, body, token)
+	res, err := requestCode(qrCodeAPI, body, token)
+	if err != nil {
+		return nil
+	}
+	defer res.Body.Close()
+
+	return saveCode(res, filename)
 }
 
 // 向微信服务器获取二维码
+// 返回 HTTP 请求实例
 func requestCode(path, body, token string) (res *http.Response, err error) {
 	api, err := url.Parse(baseURL + path)
 	if err != nil {
@@ -84,4 +108,14 @@ func requestCode(path, body, token string) (res *http.Response, err error) {
 	}
 
 	return res, errors.New("unknown error when fetch app code")
+}
+
+// 保存二维码文件
+func saveCode(res *http.Response, filename string) error {
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0666)
 }
