ebe72add563f92a80ed5b469e5152ac21a85f244
Medivhzhan <medivhzhan@gmail.com>
2018-06-11 07:49:04 +0800

    添加功能： 解密用户信息 & 整理代码

--- util/util.go
+++ util/util.go
@@ -1,8 +1,13 @@
 package util
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
+	"crypto/sha1"
+	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"net/http"
 	"net/url"
 	"sort"
@@ -67,4 +72,60 @@ func PKCS5UnPadding(plaintext []byte) []byte {
 	// 去掉最后一个字节 unPadding 次
 	unPadding := int(plaintext[ln-1])
 	return plaintext[:(ln - unPadding)]
+}
+
+// Validate 对数据包进行签名校验，确保数据的完整性。
+//
+// @rawData 不包括敏感信息的原始数据字符串，用于计算签名。
+// @signature 使用 sha1( rawData + sessionkey ) 得到字符串，用于校验用户信息
+// @ssk 微信 session_key
+func Validate(rawData, ssk, signature string) bool {
+	r := sha1.Sum([]byte(rawData + ssk))
+
+	return signature == hex.EncodeToString(r[:])
+}
+
+// Decode 解密数据
+//
+// @ssk 通过 Login 向微信服务端请求得到的 session_key
+// @data 小程序通过 api 得到的加密数据(encryptedData)
+// @iv 小程序通过 api 得到的初始向量(iv)
+func Decode(ssk, data, iv string) (bts []byte, err error) {
+
+	dSsk, err := base64.StdEncoding.DecodeString(ssk)
+	if err != nil {
+		return
+	}
+
+	ciphertext, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return
+	}
+
+	dIv, err := base64.StdEncoding.DecodeString(iv)
+	if err != nil {
+		return
+	}
+
+	block, err := aes.NewCipher(dSsk)
+	if err != nil {
+		return
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		err = errors.New("cipher too short")
+		return
+	}
+
+	// CBC mode always works in whole blocks.
+	if len(ciphertext)%aes.BlockSize != 0 {
+		err = errors.New("cipher is not a multiple of the block size")
+		return
+	}
+
+	cipher.NewCBCDecrypter(block, []byte(dIv)).CryptBlocks(ciphertext, ciphertext)
+
+	return PKCS5UnPadding([]byte(ciphertext)), nil
 }
--- weapp.go
+++ weapp.go
@@ -1,9 +1,6 @@
 package weapp
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -33,30 +30,32 @@ type Response struct {
 	Errmsg  string `json:"errmsg,omitempty"`
 }
 
-// 拼接 获取 session_key 的 URL
-func code2url(appID, secret, code string) (string, error) {
-
-	url, err := url.Parse(BaseURL + codeAPI)
-	if err != nil {
-		return "", err
-	}
-
-	query := url.Query()
-
-	query.Set("appid", appID)
-	query.Set("secret", secret)
-	query.Set("js_code", code)
-	query.Set("grant_type", "authorization_code")
-
-	url.RawQuery = query.Encode()
-
-	return url.String(), nil
-}
-
 type loginForm struct {
 	Response
 	Openid     string `json:"openid"`
 	SessionKey string `json:"session_key"`
+}
+
+// PhoneNumber 解密后的用户手机号码信息
+type PhoneNumber struct {
+	PhoneNumber     string    `json:"phoneNumber"`
+	PurePhoneNumber string    `json:"purePhoneNumber"`
+	CountryCode     string    `json:"countryCode"`
+	Watermark       watermark `json:"watermark"`
+}
+
+// Userinfo 解密后的用户信息
+type Userinfo struct {
+	OpenID    string    `json:"openId"`
+	Nickname  string    `json:"nickName"`
+	Gender    int       `json:"gender"`
+	Province  string    `json:"province"`
+	Language  string    `json:"language"`
+	Country   string    `json:"country"`
+	City      string    `json:"city"`
+	Avatar    string    `json:"avatarUrl"`
+	UnionID   string    `json:"unionId"`
+	Watermark watermark `json:"watermark"`
 }
 
 // Login 用户登录
@@ -95,14 +94,6 @@ func Login(appID, secret, code string) (string, string, error) {
 	return data.Openid, data.SessionKey, nil
 }
 
-// PhoneNumber 解密后的用户手机号码信息
-type PhoneNumber struct {
-	PhoneNumber     string    `json:"phoneNumber"`
-	PurePhoneNumber string    `json:"purePhoneNumber"`
-	CountryCode     string    `json:"countryCode"`
-	Watermark       watermark `json:"watermark"`
-}
-
 type watermark struct {
 	AppID     string `json:"appid"`
 	Timestamp int64  `json:"timestamp"`
@@ -115,44 +106,54 @@ type watermark struct {
 // @iv 小程序通过 api 得到的初始向量(iv)
 func DecodePhoneNumber(ssk, data, iv string) (phone PhoneNumber, err error) {
 
-	dSsk, err := base64.StdEncoding.DecodeString(ssk)
+	bts, err := util.Decode(ssk, data, iv)
 	if err != nil {
 		return
 	}
-
-	ciphertext, err := base64.StdEncoding.DecodeString(data)
-	if err != nil {
-		return
-	}
-
-	dIv, err := base64.StdEncoding.DecodeString(iv)
-	if err != nil {
-		return
-	}
-
-	block, err := aes.NewCipher(dSsk)
-	if err != nil {
-		return
-	}
-
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
-	if len(ciphertext) < aes.BlockSize {
-		err = errors.New("cipher too short")
-		return
-	}
-
-	// CBC mode always works in whole blocks.
-	if len(ciphertext)%aes.BlockSize != 0 {
-		err = errors.New("cipher is not a multiple of the block size")
-		return
-	}
-
-	cipher.NewCBCDecrypter(block, []byte(dIv)).CryptBlocks(ciphertext, ciphertext)
-
-	bts := util.PKCS5UnPadding([]byte(ciphertext))
 
 	err = json.Unmarshal(bts, &phone)
-
 	return
+}
+
+// DecodeUserInfo 解密用户信息
+//
+// @rawData 不包括敏感信息的原始数据字符串，用于计算签名。
+// @encryptedData 包括敏感数据在内的完整用户信息的加密数据
+// @signature 使用 sha1( rawData + sessionkey ) 得到字符串，用于校验用户信息
+// @iv 加密算法的初始向量
+// @ssk 微信 session_key
+func DecodeUserInfo(rawData, encryptedData, signature, iv, ssk string) (ui Userinfo, err error) {
+
+	if ok := util.Validate(rawData, ssk, signature); !ok {
+		err = errors.New("数据校验失败")
+		return
+	}
+
+	bts, err := util.Decode(ssk, encryptedData, iv)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(bts, &ui)
+	return
+}
+
+// 拼接 获取 session_key 的 URL
+func code2url(appID, secret, code string) (string, error) {
+
+	url, err := url.Parse(BaseURL + codeAPI)
+	if err != nil {
+		return "", err
+	}
+
+	query := url.Query()
+
+	query.Set("appid", appID)
+	query.Set("secret", secret)
+	query.Set("js_code", code)
+	query.Set("grant_type", "authorization_code")
+
+	url.RawQuery = query.Encode()
+
+	return url.String(), nil
 }
