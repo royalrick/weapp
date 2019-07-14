ef104a848da8487f00fccb6424fb0ac2b1b10bd3
Hubert Zhang <hubert.zyk@gmail.com>
2019-07-14 23:13:48 +0800

    重命名 ServeHTTP 函数，将 unmarshal 过程移出 decryptMsg

--- server.go
+++ server.go
@@ -154,8 +154,8 @@ func unmarshal(data []byte, tp dataType, v interface{}) error {
 	return nil
 }
 
-// ServeHTTP 接收并处理微信通知服务
-func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
+// HandleRequest 接收并处理微信通知服务
+func (srv *Server) HandleRequest(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case "POST":
 		raw, err := ioutil.ReadAll(r.Body)
@@ -178,14 +178,15 @@ func (srv *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
 			if !validateSignature(signature, srv.token, timestamp, nonce, res.Encrypt) {
 				return errors.New("invalid signature")
 			}
-			err := srv.decryptMsg(res.Encrypt, tp, mix)
+			body, err := srv.decryptMsg(res.Encrypt)
 			if err != nil {
 				return err
 			}
-		} else {
-			if err := unmarshal(raw, tp, mix); err != nil {
-				return err
-			}
+			length := binary.BigEndian.Uint32(body[16:20])
+			raw = body[20 : 20+length]
+		}
+		if err := unmarshal(raw, tp, mix); err != nil {
+			return err
 		}
 
 		ok := false // 是否已经收到消息
@@ -312,31 +313,18 @@ func (srv *Server) encryptMsg(message, nonce string, timestamp int) (*EncryptedM
 }
 
 // 检验消息的真实性，并且获取解密后的明文.
-func (srv *Server) decryptMsg(encrypted string, tp dataType, msg *Mixture) error {
+func (srv *Server) decryptMsg(encrypted string) ([]byte, error) {
 
 	key, err := srv.getAESKey()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	ciphertext, err := base64.StdEncoding.DecodeString(encrypted)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	data, err := cbcDecrypt(key, ciphertext, key)
-	if err != nil {
-		return err
-	}
-
-	// TODO: old
-	// 	if err := unmarshal(data[:], msg, srv.dataType()); err != nil {
-	// 		return err
-	// 	}
-	length := binary.BigEndian.Uint32(data[16:20])
-	if err := unmarshal(data[20:20+length], tp, msg); err != nil {
-		return err
-	}
-
-	return nil
+	return data, err
 }
