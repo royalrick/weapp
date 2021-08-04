5e7173826c26d2d4dceb3c0c5f520ef0d5810542
Medivh <medivhzhan@gmail.com>
2021-08-04 09:34:15 +0800

    优化 cache 包命名

--- cache/cache.go
+++ cache/cache.go
@@ -1,48 +1,11 @@
 package cache
 
-import (
-	"sync"
-	"time"
-)
+import "time"
 
-type memory struct {
-	store *sync.Map
-}
-
-type memoryItem struct {
-	val   interface{}
-	timer *time.Timer
-}
-
-func NewMemoryCache() Cache {
-	mem := memory{
-		store: new(sync.Map),
-	}
-
-	return &mem
-}
-
-func (c *memory) Get(key string) (interface{}, bool) {
-	item, ok := c.store.Load(key)
-	if !ok {
-		return nil, false
-	}
-
-	return item.(memoryItem).val, true
-}
-
-func (c *memory) Set(key string, val interface{}, timeout time.Duration) {
-	timer := time.AfterFunc(timeout, func() {
-		_ = c.Delete(key)
-
-	})
-
-	item := memoryItem{val, timer}
-
-	c.store.Store(key, item)
-}
-
-func (c *memory) Delete(key string) error {
-	c.store.Delete(key)
-	return nil
+type Cache interface {
+	// 存储数据
+	Set(key string, val interface{}, timeout time.Duration)
+	// 获取数据
+	// 返回数据和数据是否存在
+	Get(key string) (interface{}, bool)
 }
