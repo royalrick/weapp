e0822a9e63264226fff4412f9621dd30c0b7e833
Medivh <medivhzhan@gmail.com>
2021-09-26 17:15:48 +0800

    调整`解密后的用户信息`内容

--- decrypt.go
+++ decrypt.go
@@ -89,16 +89,27 @@ func (cli *Client) DecryptShareInfo(sessionKey, encryptedData, iv string) (*Shar
 
 // UserInfo 解密后的用户信息
 type UserInfo struct {
-	OpenID    string    `json:"openId"`
-	Nickname  string    `json:"nickName"`
-	Gender    int       `json:"gender"`
-	Province  string    `json:"province"`
-	Language  string    `json:"language"`
-	Country   string    `json:"country"`
-	City      string    `json:"city"`
-	Avatar    string    `json:"avatarUrl"`
-	UnionID   string    `json:"unionId"`
-	Watermark watermark `json:"watermark"`
+	// 用户昵称
+	// 用户头像图片的 URL。URL 最后一个数值代表正方形头像大小（有 0、46、64、96、132 数值可选，0 代表 640x640 的正方形头像，46 表示 46x46 的正方形头像，剩余数值以此类推。默认132），用户没有头像时该项为空。若用户更换头像，原有头像 URL 将失效。
+	Avatar string `json:"avatarUrl"`
+	// 用户性别
+	// 0	未知
+	// 1	男性
+	// 2	女性
+	Gender int `json:"gender"`
+	// 用户所在国家
+	Country string `json:"country"`
+	// 用户所在城市
+	City string `json:"city"`
+	// 显示 country，province，city 所用的语言
+	// 	en	英文
+	// zh_CN	简体中文
+	// zh_TW	繁体中文
+	Language string `json:"language"`
+	// 用户昵称
+	Nickname string `json:"nickName"`
+	// 用户所在省份
+	Province string `json:"province"`
 }
 
 // DecryptUserInfo 解密用户信息
