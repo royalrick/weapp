e10f98598a106de33c6fd47eb432719f33505baa
Rick <medivhzhan@icloud.com>
2022-03-23 20:01:45 +0800

    接口 获取小程序码 增加返回字段

--- wxacode/get_unlimited.go
+++ wxacode/get_unlimited.go
@@ -16,6 +16,10 @@ type UnlimitedQRCode struct {
 	AutoColor bool   `json:"auto_color,omitempty"`
 	LineColor Color  `json:"line_color,omitempty"`
 	IsHyaline bool   `json:"is_hyaline,omitempty"`
+	// 检查 page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但 page 有数量上限（60000个）请勿滥用
+	EnvVersion bool `json:"env_version,omitempty"`
+	// 要打开的小程序版本。正式版为 release，体验版为 trial，开发版为 develop
+	CheckPath bool `json:"check_path,omitempty"`
 }
 
 // 获取小程序码，适用于需要的码数量极多的业务场景。通过该接口生成的小程序码，永久有效，数量暂无限制。
