76b4110f719e55fc9489506c6a16c51aae6dded2
Rick <medivhzhan@gmail.com>
2022-03-27 09:10:47 +0800

    fix: 修复获取小程序码功能

--- wxacode/get_unlimited.go
+++ wxacode/get_unlimited.go
@@ -19,7 +19,7 @@ type UnlimitedQRCode struct {
 	// 要打开的小程序版本。正式版为 release，体验版为 trial，开发版为 develop
 	EnvVersion string `json:"env_version,omitempty"`
 	// 检查 page 是否存在，为 true 时 page 必须是已经发布的小程序存在的页面（否则报错）；为 false 时允许小程序未发布或者 page 不存在， 但 page 有数量上限（60000个）请勿滥用
-	CheckPath bool `json:"check_path,omitempty"`
+	CheckPath bool `json:"check_path"`
 }
 
 // 获取小程序码，适用于需要的码数量极多的业务场景。通过该接口生成的小程序码，永久有效，数量暂无限制。
