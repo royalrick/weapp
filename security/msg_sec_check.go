26bda2d493918f88b118bd729baebed73607aeac
Rick <medivhzhan@icloud.com>
2022-05-21 07:55:44 +0800

    fix: 修复内容检测返回结果

--- security/msg_sec_check.go
+++ security/msg_sec_check.go
@@ -30,7 +30,7 @@ type MsgSecCheckResponse struct {
 		// 建议，有risky、pass、review三种值
 		Suggest string `json:"suggest"`
 		// 命中标签枚举值，100 正常；10001 广告；20001 时政；20002 色情；20003 辱骂；20006 违法犯罪；20008 欺诈；20012 低俗；20013 版权；21000 其他
-		Label string `json:"label"`
+		Label int `json:"label"`
 	} `json:"result"`
 	// 详细检测结果
 	Detail []struct {
